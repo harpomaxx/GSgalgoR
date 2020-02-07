e1d0873531e62aefc2047ea3d399fa85e7bae002
martinguerrero89 <martinguerrero89@users.noreply.github.com>
2020-02-07 17:33:12 -0300

    Update galgo_main.R

--- R/galgo_main.R
+++ R/galgo_main.R
@@ -472,16 +472,14 @@ base_save_pop_partial_callback <- function(directory="results/"){
     environment(base_save_pop_callback)<-environment()
     base_save_pop_callback(directory,prefix=g)
   }
-  #output <- list(Solutions = X1, ParetoFront = PARETO)
-  #class(output)= "galgo.Obj"
-  #return(output)
+
 }
 
 base_save_pop_final_callback <- function(directory="results/"){
   if (!dir.exists(directory))
     dir.create(directory)
   environment(base_save_pop_callback)<-environment()
-  base_save_pop_callback(directory,prefix="final")
+  output<- base_save_pop_callback(directory,prefix="final")
   print(paste0("final population saved in final.rda"))
   class(output)<- "galgo.Obj"
   return(output)
