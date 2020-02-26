19344c4cdccc1e80464a81264022cdb530706144
Harpo MAxx <harpomaxx@gmail.com>
2020-02-26 14:49:49 +0000

    fix path problem with example dataset
    add support for downloading the dataset from gh

--- R/install_data.R
+++ R/install_data.R
@@ -5,5 +5,9 @@
 #'
 #' @examples
 use_rna_luad <- function(){
-  data<-readRDS("./data/luad_data.rds")
+  dest_dir <- paste(find.package("galgoR"),"/data",sep="")
+  dest_file <- paste(dest_dir,"/luad_data.rds",sep="")
+  if (! file.exists(dest_file))
+      download.file("https://github.com/harpomaxx/galgo/raw/0aaa1f070715b2dea541eb1a80691c982e49984f/data/luad_data.rds",dest_file)
+  data<-readRDS(dest_file)
 }
