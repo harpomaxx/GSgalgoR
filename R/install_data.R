2323b99805a861924a1ee2e499d86153c7036fec
Harpo MAxx <harpomaxx@gmail.com>
2020-03-02 13:59:48 +0000

    Add namespace for download file function

--- R/install_data.R
+++ R/install_data.R
@@ -8,6 +8,6 @@ use_rna_luad <- function(){
   dest_dir <- paste(find.package("galgoR"),"/data",sep="")
   dest_file <- paste(dest_dir,"/luad_data.rds",sep="")
   if (! file.exists(dest_file))
-      download.file("https://github.com/harpomaxx/galgo/raw/0aaa1f070715b2dea541eb1a80691c982e49984f/data/luad_data.rds",dest_file)
+      utils::download.file("https://github.com/harpomaxx/galgo/raw/0aaa1f070715b2dea541eb1a80691c982e49984f/data/luad_data.rds",dest_file)
   data<-readRDS(dest_file)
 }
